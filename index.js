abad1ee6798577157c56751ff5e1ae52fd6f344c
emadbakry <101658737+emadbakry@users.noreply.github.com>
2023-01-13 07:03:06 +0300

    fix_problems

--- index.js
+++ index.js
@@ -52,8 +52,6 @@ let lastX = 0;
 let lastY = 0; 
 let hue = 0;
 let draw = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
     if (drawingNow) {
         // console.log(e)
         ctx.strokeStyle = `hsl(#{hue}, 100%,50%)`;
@@ -97,6 +95,44 @@ save_btn.addEventListener('click', () => {
     }, 1000);
 });
 
+// To prevent scrolling while touching canvas
+document.body.addEventListener("touchstart", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchend", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchmove", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+
+canvas.addEventListener(
+  "touchstart",
+  function (e) {
+    mousePos = getTouchPos(canvas, e);
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mousedown", {
+      clientX: touch.clientX,
+      clientY: touch.clientY,
+    });
+    canvas.dispatchEvent(mouseEvent);
+  },
+  false
+);
+canvas.addEventListener(
+  "touchend",
+  function (e) {
+    var mouseEvent = new MouseEvent("mouseup", {});
+    canvas.dispatchEvent(mouseEvent);
+  },
+  false
+);
 canvas.addEventListener(
   "touchmove",
   function (e) {
@@ -109,6 +145,15 @@ canvas.addEventListener(
   },
   false
 );
+function getTouchPos(canvasDom, touchEvent) {
+  var rect = canvasDom.getBoundingClientRect();
+  return {
+    x: touchEvent.touches[0].clientX - rect.left,
+    y: touchEvent.touches[0].clientY - rect.top
+  };
+}
+
+
 let reset_bg = document.querySelector(".btns .reset_btn");
 let reset = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
